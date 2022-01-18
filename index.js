8a019d5d531820683a0bd54fd78e6ed22543ff5b
shantanusoam <51102185+shantanusoam@users.noreply.github.com>
2022-01-18 14:39:25 -0800

    send

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
 const postRoute = require('./routes/posts');
 const categoryRoute = require('./routes/categories');
-const multer = require('multer');
+// const multer = require('multer');
 const path = require('path');
 const cors = require('cors');
 
@@ -25,24 +25,24 @@ mongoose
   .then(console.log('Connected to MongoDB'))
   .catch((err) => console.log(err));
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'images');
-  },
-  filename: (req, file, cb) => {
-    cb(null, req.body.name);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'images');
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, req.body.name);
+//   },
+// });
 
-const upload = multer({ storage: storage });
-app.post('/api/upload', upload.single('file'), (req, res) => {
-  res.status(200).json('File has been uploaded');
-});
+// const upload = multer({ storage: storage });
+// app.post('/api/upload', upload.single('file'), (req, res) => {
+//   res.status(200).json('File has been uploaded');
+// });
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
 app.use('/api/posts', postRoute);
-app.use('/api/categories', categoryRoute);
+// app.use('/api/categories', categoryRoute);
 
 app.listen(port, () => {
   console.log(`Backend is running. at ${port}`);
--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,9 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const bcrypt = require("bcrypt");
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
 
 //REGISTER
-router.post("/register", async (req, res) => {
+router.post('/register', async (req, res) => {
   try {
     const salt = await bcrypt.genSalt(10);
     const hashedPass = await bcrypt.hash(req.body.password, salt);
@@ -21,13 +21,13 @@ router.post("/register", async (req, res) => {
 });
 
 //LOGIN
-router.post("/login", async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-    !user && res.status(400).json("Wrong credentials!");
+    !user && res.status(400).json('Wrong credentials!');
 
     const validated = await bcrypt.compare(req.body.password, user.password);
-    !validated && res.status(400).json("Wrong credentials!");
+    !validated && res.status(400).json('Wrong credentials!');
 
     const { password, ...others } = user._doc;
     res.status(200).json(others);
